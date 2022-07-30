3445e4f5546f0b46d00a169f6f7fc9d4bbb9c9b4
Arsen <lisnychukarsenw@gmail.com>
2022-07-30 14:52:29 +0300

    add action for testing and Serialization

--- Character system/CharacterObject/CharacterObject.cs
+++ Character system/CharacterObject/CharacterObject.cs
@@ -1,19 +1,54 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Character_system.Character
 {
-    struct Attribute
+    [Serializable]
+    class CharacterAttribute
     {
-        string name;
-        object value;
+        public string name;
+        public object value;
+        public CharacterAttribute(string name=null, object value=null)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return name + ": " + value.ToString();
+        }
     }
 
+    [Serializable]
     class CharacterObject
     {
         public string name;
         public string description;
         public float weight;
-        public List<Attribute> attributes;
+        public List<CharacterAttribute> attributes;
+        public CharacterObject(string name = null, string description = null, float weight = 0, List<CharacterAttribute> attributes = null)
+        {
+            this.name = name;
+            this.description = description;
+            this.weight = weight;
+            this.attributes = attributes;
+        }
+
+        public override string ToString()
+        {
+            string str =
+                 "name: " + name + '\n' +
+                "description: " + description + '\n' +
+                "weight: " + weight + '\n' + 
+                "attributes:" + '\n';
+            string attributes = "";
+            foreach (var attribute in this.attributes)
+            {
+                attributes += '\t' + attribute.ToString() + '\n';
+            }
+            return str + attributes;
+        }
     }
 }
 
