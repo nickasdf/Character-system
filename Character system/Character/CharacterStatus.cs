237b3af9777bde69da4830005c71fbc2a62fa211
Микола Ковальський <mykola200410@gmail.com>
2022-07-31 12:50:15 -0400

    був доданий клас який ініціалізує ліст всіхатрибутів

--- Character system/Character/CharacterStatus.cs
+++ Character system/Character/CharacterStatus.cs
@@ -8,6 +8,47 @@ namespace Character_system
 {
     internal class CharacterStatus
     {
+        List<Character.CharacterAttribute> allAttributes;
 
+        public CharacterStatus()
+        {
+            allAttributes = new List<Character.CharacterAttribute>();
+        }
+        public bool Equals(Character.CharacterAttribute attribute1, Character.CharacterAttribute attribute2)
+        {
+            if (attribute1.name == attribute2.name)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Fillstatus(Character.Character character)
+        {
+            allAttributes.Clear();
+            foreach (var item in character.items)
+            {
+                foreach (var attribute in item.attributes)
+                {
+                    if (allAttributes.Count == 0)
+                    {
+                        allAttributes.Add(attribute);
+                    }
+                    else
+                    {
+                        foreach (var uniqueAttribute in allAttributes)
+                        {
+                            if (Equals(uniqueAttribute, attribute))
+                            {
+                                uniqueAttribute.value += attribute.value;
+                            }
+                            else
+                            {
+                                allAttributes.Add(attribute);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
