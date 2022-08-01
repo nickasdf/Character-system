3fe12b76424f56f93f3b5ceeb940fc4064c92f09
Микола Ковальський <mykola200410@gmail.com>
2022-07-31 22:33:41 -0400

    вивів всі атрибути на прау панель тестового ліста

--- Character system/UI/CharacterPage.xaml.cs
+++ Character system/UI/CharacterPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
+using System.Collections.Generic;
 using System.Windows.Controls;
-
+using System.Data;
+using Character_system.Character;
 namespace Character_system.UI
 {
     /// <summary>
@@ -8,11 +10,32 @@ namespace Character_system.UI
     /// </summary>
     public partial class CharacterPage : Page
     {
+
         public CharacterPage()
         {
             InitializeComponent();
+
+
+            List<Character.CharacterAttribute> allAttributes = new List<Character.CharacterAttribute>();
+
+            allAttributes = new List<CharacterAttribute>();
+            addObject(allAttributes,40);
+            
+
+            LstAttributes.ItemsSource = allAttributes;
+
+
         }
 
+        void addObject(List<CharacterAttribute> lst , int  number = 3 )
+        {
+            for (int i = 0; i < number; i++)
+            {
+                lst.Add(new CharacterAttribute() { name = "Speed", value = 10 });
+                lst.Add(new CharacterAttribute() { name = "ebat", value = 10 });
+                lst.Add(new CharacterAttribute() { name = "NIHUASEBE", value = 10 });
+            } 
+        }
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             /*ListBoxItem listBoxItem = new ListBoxItem();
